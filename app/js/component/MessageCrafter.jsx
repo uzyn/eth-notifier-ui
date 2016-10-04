bc2d96ca85564e895a3f0cade8429a9e198938fc
U-Zyn Chua <chua@uzyn.com>
2016-10-04 15:37:31 +0800

    Switched to state from ref

--- app/js/component/MessageCrafter.jsx
+++ app/js/component/MessageCrafter.jsx
@@ -7,16 +7,20 @@ const propTypes = {
 class MessageCrafter extends React.Component {
   constructor(props, context) {
     super(props, context);
+
+    this.state = {
+      destination: '',
+      message: '',
+      method: 'xipfs-encrypted',
+    };
     this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleChange = this.handleChange.bind(this);
   }
 
   handleSubmit(event) {
     event.preventDefault();
-    console.log(this.phoneInput.value);
-    console.log(this.messageInput.value);
-    console.log(this.crafterForm.method.value);
 
-    switch (this.crafterForm.method.value) {
+    switch (this.state.method) {
       case 'xipfs-plain':
       case 'xipfs-encrypted': {
         const xhr = new XMLHttpRequest();
@@ -29,9 +33,9 @@ class MessageCrafter extends React.Component {
           }
         };
         xhr.send(JSON.stringify({
-          destination: this.phoneInput.value.trim(),
-          message: this.messageInput.value.trim(),
-          method: this.crafterForm.method.value,
+          destination: this.state.destination.trim(),
+          message: this.state.message.trim(),
+          method: this.state.method,
         }));
         break;
       }
@@ -41,42 +45,60 @@ class MessageCrafter extends React.Component {
     }
   }
 
+  handleChange(event) {
+    // Only support phone for the time being, automatically prepend '+'
+    let value = event.target.value;
+    if (event.target.name === 'destination') {
+      if (value.length > 0 && value.substring(0, 1) !== '+') {
+        value = `+${value}`;
+      }
+    }
+
+    this.setState({ [event.target.name]: value });
+  }
+
   render() {
     return (
-      <form className="form" ref={(ref) => { this.crafterForm = ref; }}>
+      <form className="form">
         <div className="form-group">
           <label htmlFor="phone">Phone number</label>
-          <input type="text" className="form-control" id="phone" placeholder="+15417543010" ref={(ref) => { this.phoneInput = ref; }} />
+          <input type="text" className="form-control" id="destination" name="destination" placeholder="e.g.: +15417543010" value={this.state.destination} onChange={this.handleChange} />
+          <p className="help-block">Phone number in E.164 format starting with + and country code.</p>
         </div>
         <div className="form-group">
           <label htmlFor="message">Message</label>
-          <input type="message" className="form-control" id="message" placeholder="Hello..." ref={(ref) => { this.messageInput = ref; }} />
+          <input type="message" className="form-control" id="message" name="message" placeholder="Hello..." value={this.state.message} onChange={this.handleChange} />
         </div>
 
         <p><strong>Via</strong></p>
         <div className="radio">
           <label>
-            <input type="radio" name="method" id="methodStandard" value="standard" defaultChecked />
+            <input type="radio" name="method" id="methodStandard" value="standard" checked={this.state.method === 'standard'} onChange={this.handleChange} />
             Standard Ethereum call<br />
             <small className="radio-explanation">Parameters are publicly visible on blockchain.</small>
           </label>
         </div>
         <div className="radio">
           <label>
-            <input type="radio" name="method" id="methodxIPFSPlain" value="xipfs-plain" />
+            <input type="radio" name="method" id="methodxIPFSPlain" value="xipfs-plain" checked={this.state.method === 'xipfs-plain'} onChange={this.handleChange} />
             IPFS-augmented plaintext call<br />
             <small className="radio-explanation">Lower gas, data remains publicly viewable.</small>
           </label>
         </div>
         <div className="radio">
           <label>
-            <input type="radio" name="method" id="methodxIPFSEncrypted" value="xipfs-encrypted" />
+            <input type="radio" name="method" id="methodxIPFSEncrypted" value="xipfs-encrypted" checked={this.state.method === 'xipfs-encrypted'} onChange={this.handleChange} />
             IPFS-augmented call with encryption<br />
             <small className="radio-explanation">Lower gas, data is encrypted.</small>
           </label>
         </div>
 
         <button type="submit" className="btn btn-default btn-lg" onClick={this.handleSubmit}>Send notification</button>
+        <span className="after-button">
+          <span className="error">osidjofi</span>
+          <span className="success">sdfkpsodkfpoksf</span>
+        </span>
+
       </form>
     );
   }
